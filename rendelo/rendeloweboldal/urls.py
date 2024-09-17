19fd5d331a5cf0aeefb1ad879ede10d151e9a90a
sanyopapa <sandormarton265@gmail.com>
2024-09-17 18:45:15 +0200

    backend kesz

--- rendelo/rendeloweboldal/urls.py
+++ rendelo/rendeloweboldal/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.kezdooldal, name='kezdooldal'),
+    path('', views.kezdooldal, name='home'),  
+    path('book/', views.idopontfoglalas, name='book'),
+    path('admin-view/', views.admin_view, name='admin_view'),  
 ]
--- rendelo/rendeloweboldal/views.py
+++ rendelo/rendeloweboldal/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def kezdooldal(request):
     return render(request, 'kezdooldal.html')
+
+def idopontfoglalas(request):
+    return render(request, 'idopontfoglalas.html')
+
+def admin_view(request):
+    return render(request, 'admin.html')
