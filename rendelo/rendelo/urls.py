c15226dfc9ab052b0c17cd12cee623b3e962c057
sanyopapa <sandormarton265@gmail.com>
2024-09-18 18:33:46 +0200

    frontend egyenlore kesz

--- rendelo/rendelo/urls.py
+++ rendelo/rendelo/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from rendeloweboldal.views import register_view, login_view
 urlpatterns = [
     path('admin/', admin.site.urls),  
     path('', include('rendeloweboldal.urls')),  
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
 ]
--- rendelo/rendeloweboldal/views.py
+++ rendelo/rendeloweboldal/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Appointment
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 def kezdooldal(request):
     return render(request, 'kezdooldal.html')
@@ -16,3 +18,26 @@ def idopontfoglalas(request):
 
 def admin_view(request):
     return render(request, 'admin.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
