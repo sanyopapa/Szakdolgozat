ab2eb8bf345a5289ce46d0ae0936c6e6c70c8094
sanyopapa <sandormarton265@gmail.com>
2024-09-18 15:32:22 +0200

    .

--- rendelo/rendeloweboldal/models.py
+++ rendelo/rendeloweboldal/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Appointment(models.Model):
+    datetime = models.DateTimeField()
+    treatment = models.CharField(max_length=100)
--- rendelo/rendeloweboldal/views.py
+++ rendelo/rendeloweboldal/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Appointment
 
 def kezdooldal(request):
     return render(request, 'kezdooldal.html')
 
 def idopontfoglalas(request):
+    if request.method == 'POST':
+        appointment_datetime = request.POST['appointment_datetime']
+        treatment = request.POST['treatment']
+        # Itt mentem az adatbázisba
+        Appointment.objects.create(datetime=appointment_datetime, treatment=treatment)
+        return redirect('home')
     return render(request, 'idopontfoglalas.html')
 
 def admin_view(request):
